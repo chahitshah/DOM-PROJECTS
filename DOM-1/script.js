d59262be14b7d920d6d5d373e0e7193d0a2b7426
Chahit Shah <shahchahit2004@gmail.com>
2026-04-12 22:49:32 +0530

    feat: add day-26 dashboard with todo system and page navigation

--- DOM-1/script.js
+++ DOM-1/script.js
@@ -1,4 +1,5 @@
-let Allcards = document.querySelectorAll(".cards");
+function OpenPage(){
+    let Allcards = document.querySelectorAll(".cards");
 let Allpages = document.querySelectorAll(".Pages")
 let btn = document.querySelectorAll("button")
 
@@ -13,4 +14,56 @@ btn.forEach(function(elem){
     elem.addEventListener("click",function(){
         Allpages[elem.id].style.display="none";
     })
+})
+}
+
+OpenPage();
+
+let form = document.querySelector(".addtask form")
+let taskinput = document.querySelector(".addtask form .Entertask ");
+let taskdetailsinput = document.querySelector(".addtask form textarea");
+let checkboxinput = document.querySelector(".addtask form #mark-imp");
+
+let currenttask = [
+    {
+        task:"mandir jao",
+        details:'dfdf',
+        imp:true
+    },
+    {
+        task:"mandir jao",
+        details:'dfdf',
+        imp:false
+    }
+]
+
+function renderTask()
+{
+
+let alltask = document.querySelector(".alltask");
+var sum = '';
+
+currenttask.forEach(function(elem)
+{
+    sum+=`<div class="task">
+                        <h5>${elem.task} <span class="${elem.imp}">Imp</span></h5>
+                        <button>Mark as Completed</button>
+                    </div>`
+})
+
+alltask.innerHTML=sum;
+}
+
+renderTask();
+
+form.addEventListener("submit",function(e){
+    e.preventDefault();
+    
+
+    currenttask.push({task:taskinput.value,details:taskdetailsinput.value,imp:checkboxinput.checked});
+    taskinput.value='';
+    taskdetailsinput.value='';
+    checkboxinput.checked='';
+
+    renderTask();
 })
